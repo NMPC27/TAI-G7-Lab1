44a2ba65eb7e6fe75ca932f17eaf6d5d3a9cdaee
Martinho Tavares <martinho.tavares@ua.pt>
2023-03-27 20:52:00 +0100

    Updated scripts for parametrized input

--- scripts/csv_to_information.py
+++ scripts/csv_to_information.py
@@ -5,6 +5,12 @@ from math import log2
 from argparse import ArgumentParser
 
 
+"""
+Calculate information and sliding entropy at each step, as stored in .csv files,
+and output it into the 'processed' folder.
+"""
+
+
 def get_approximate_byte_count(file_name: str) -> int:
     file_stat = stat(file_name)
     return file_stat.st_size
@@ -37,13 +43,19 @@ def get_information(file_name: str, max_size: int) -> np.ndarray:
     return information_steps
 
 def process_and_save_information_and_entropy(main_file_name: str):
-    source_file_path = f'../example/{main_file_name}.txt'
-    results_file_path = f'./input/{main_file_name}.csv'
-    processed_information_file_path = f'./processed/{main_file_name}_information.npy'
-    processed_entropy_file_path = f'./processed/{main_file_name}_entropy.npy'
+    main_file_parameters = os.path.basename(main_file_name).split('_')
+    source_file_name = '_'.join(main_file_parameters[:len(main_file_parameters) - 5])
 
+    source_file_path = f'../example/{source_file_name}.txt'
+    results_file_path = main_file_name
+    
     assert os.path.exists(source_file_path), f'Source file \'{source_file_path}\' doesn\'t exist!'
     assert os.path.exists(results_file_path), f'Source file \'{results_file_path}\' doesn\'t exist!'
+    
+    results_file_path_without_extension, _ = os.path.splitext(os.path.basename(results_file_path))
+    processed_information_file_path = f'./processed/{results_file_path_without_extension}_information.npy'
+    processed_entropy_file_path = f'./processed/{results_file_path_without_extension}_entropy.npy'
+
 
     max_number_of_characters = get_approximate_byte_count(source_file_path)
 
@@ -59,8 +71,9 @@ def process_and_save_information_and_entropy(main_file_name: str):
 if __name__ == '__main__':
 
     parser = ArgumentParser(__file__)
-    parser.add_argument('file', type=str)
+    parser.add_argument('file', nargs='+', type=str)
 
     args = parser.parse_args()
 
-    process_and_save_information_and_entropy(args.file)
+    for fname in args.file:
+        process_and_save_information_and_entropy(fname)
